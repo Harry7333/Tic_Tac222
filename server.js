4d6acfbdac613b17aae114ab6746fdd360360622
Harry7333 <harish.sorada@gmail.com>
2025-02-17 11:59:15 +0530

    add port
    add port

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
